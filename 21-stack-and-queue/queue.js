5c2f8744dc0f4487904ac5b305481d2a62c8dc10
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-28 17:21:13 +0700

    Queue implementation

---
+++ 21-stack-and-queue/queue.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        const node = new Node(val);
+
+        if(!this.first) {
+            this.first = node;
+            this.last = node;
+        } else {
+            this.last.next = node;
+            this.last = node;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if(!this.first) return null;
+
+        let temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+
+        return temp.value;
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(123);
+queue.enqueue(456);
+queue.enqueue(789);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
