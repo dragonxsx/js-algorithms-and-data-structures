633827328cda83cec7b044a82d05f0d4bcf84e96
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-09-04 19:48:41 +0700

    Leetcode - Container with most water

---
+++ 30-the-wild-west/02-container-with-most-water.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/container-with-most-water/
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+// n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). 
+// Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+// Notice that you may not slant the container.
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+// In this case, the max area of water (blue section) the container can contain is 49.
+
+function maxContainerArea(arr) {
+    let maxArea = 0;
+    let aIndex = 0;
+    let bIndex = arr.length - 1;
+
+    while(aIndex <= bIndex) {
+        const area = Math.min(arr[aIndex], arr[bIndex]) * Math.abs(aIndex - bIndex);
+        maxArea = Math.max(area, maxArea);
+
+        if (arr[aIndex] < arr[bIndex]) {
+            aIndex++;
+        } else {
+            bIndex--;
+        }
+    }
+
+    return maxArea;
+}
+
+console.log(maxContainerArea([1,8,6,2,5,4,8,3,7]));
